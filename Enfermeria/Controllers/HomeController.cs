68f558051c537bd3059f272022e8fa5c76de5f5a
development6 <40081976+development6@users.noreply.github.com>
2019-12-01 18:46:24 -0300

    primer commit

---
+++ Enfermeria/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Enfermeria.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ContentResult AjaxMethod(string paciente)
+        {
+            string query = "select FE_Indicadores.indicador, FE_Indicadores.inicio, FE_Indicadores.final, COUNT(Ficha_Enfermeria.id_ficha_enf)";
+            query += "from FE_Indicadores Inner join Ficha_Enfermeria On Ficha_Enfermeria.id_ficha_enf=Fe_Indicadores.id_ficha_enf Inner join Ficha on Ficha_Enfermeria.id_ficha = Ficha.id_ficha inner join Paciente on Paciente.id_paciente = Ficha.id_paciente where Paciente.id_paciente = @IdPaciente GROUP BY FE_Indicadores.indicador, FE_Indicadores.inicio, FE_Indicadores.final";
+            //string query = "select K.ergo_vol_ing, K.ergo_voml_ing, K.ergo_fcmax_ing, K.ergo_vol_egr, K.ergo_voml_egr, K.ergo_fcmax_egr, COUNT(K.id_ficha_kine)";
+            //query += "FROM Ficha_Kinesiologia K, Ficha F, Paciente Pa Where K.id_ficha = F.id_ficha And F.id_paciente ='" + paciente + "'group by K.ergo_vol_ing, K.ergo_voml_ing, K.ergo_fcmax_ing, K.ergo_vol_egr, K.ergo_voml_egr, K.ergo_fcmax_egr";
+
+            string constr = ConfigurationManager.ConnectionStrings["ConexionKaplan"].ConnectionString;
+            StringBuilder sb = new StringBuilder();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@IdPaciente", paciente);
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sb.Append("[");
+
+                        while (sdr.Read())
+                        {
+                            sb.Append("{");
+                            System.Threading.Thread.Sleep(50);
+                            string color = String.Format("#{0:X6}", new Random().Next(0x1000000));
+                            sb.Append(string.Format("value0:{0},value1:{1},value2:{2}", sdr[0], sdr[1], sdr[2]));
+                            sb.Append("},");
+                        }
+
+                        sb = sb.Remove(sb.Length - 1, 1);
+                        sb.Append("]");
+
+                    }
+
+                    con.Close();
+                }
+            }
+
+            return Content(sb.ToString());
+        }
+
+
+
+    }
+}
